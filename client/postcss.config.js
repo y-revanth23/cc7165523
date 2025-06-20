4c21db6159b1858f191e409ca1bf9996a275cc69
y-revanth23 <revanthy2006@gmail.com>
2025-06-20 09:13:32 +0530

    env

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,7 +1,6 @@
-import tailwindcss from "@tailwindcss/vite";
-export default{
-    plugins: {
-        "@tailwindcss/postcss": {},
-        autoprefixer: {},
-    },
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
+export default {
+  plugins: [tailwindcss, autoprefixer],
 };
