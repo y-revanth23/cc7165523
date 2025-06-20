7438a9555eda9ba478ad93c8e340ee3babda5d25
y-revanth23 <revanthy2006@gmail.com>
2025-06-20 08:55:08 +0530

    jk

--- server/models/Product.js
+++ server/models/Product.js
@@ -26,4 +26,4 @@ const productSchema = new mongoose.Schema({
   }
 }, { timestamps: true });
 
-module.exports = mongoose.model('Product', productSchema);
+module.exports = mongoose.model('product', productSchema);
