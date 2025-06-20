a4a4588df304a297dc2a61ecc3351e5ef9ec4afe
y-revanth23 <revanthy2006@gmail.com>
2025-06-20 08:36:16 +0530

    product

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,11 +1,29 @@
 const mongoose = require('mongoose');
+
 const productSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  category: String,
-  rating: String,
-  description: String,
-  image: String,
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  rating: {
+    type: Number,
+    default: 0
+  },
+  description: {
+    type: String
+  },
+  image: {
+    type: String
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Product', productSchema);
